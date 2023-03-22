959692072b4241808f5a29373ba137d802a8acff
FilippoFoti <filippofoti7@gmail.com>
2023-03-22 13:18:22 +0100

    Creata Array studenti

---
+++ js/script.js
@@ -0,0 +1,41 @@
+// Array studenti
+const students = [
+    {
+        nome: "Wayne",
+        cognome: "Barnett",
+        ruolo: "Founder & CEO",
+        foto: "../img/wayne-barnett-founder-ceo.jpg",
+    },
+    {
+        nome: "Angela",
+        cognome: "Caroll",
+        ruolo: "Chief Editor",
+        foto: "../img/angela-caroll-chief-editor.jpgjpg",
+    },
+    {
+        nome: "Walter",
+        cognome: "Gordon",
+        ruolo: "Office Manager",
+        foto: "../img/walter-gordon-office-manager.jpg",
+    },
+    {
+        nome: "Angela ",
+        cognome: "Lopez",
+        ruolo: "Social Media Manager",
+        foto: "../img/angela-lopez-social-media-manager.jpg",
+    },
+    {
+        nome: "Scott",
+        cognome: "Estrada",
+        ruolo: "Developer",
+        foto: "../img/scott-estrada-developer.jpg",
+    },
+    {
+        nome: "Barbara",
+        cognome: "Ramos",
+        ruolo: "Graphic Designer",
+        foto: "../img/barbara-ramos-graphic-designer.jpg",
+    },
+];
+
+console.log(students);
