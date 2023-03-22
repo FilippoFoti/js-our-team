9ce5a1d9e68a5e4e5c34fcb30cb3954cf6b64e45
FilippoFoti <filippofoti7@gmail.com>
2023-03-22 13:44:51 +0100

    Consegna e bonus completati

--- js/script.js
+++ js/script.js
@@ -4,38 +4,58 @@ const students = [
         nome: "Wayne",
         cognome: "Barnett",
         ruolo: "Founder & CEO",
-        foto: "../img/wayne-barnett-founder-ceo.jpg",
+        foto: "wayne-barnett-founder-ceo.jpg",
     },
     {
         nome: "Angela",
         cognome: "Caroll",
         ruolo: "Chief Editor",
-        foto: "../img/angela-caroll-chief-editor.jpgjpg",
+        foto: "angela-caroll-chief-editor.jpg",
     },
     {
         nome: "Walter",
         cognome: "Gordon",
         ruolo: "Office Manager",
-        foto: "../img/walter-gordon-office-manager.jpg",
+        foto: "walter-gordon-office-manager.jpg",
     },
     {
         nome: "Angela ",
         cognome: "Lopez",
         ruolo: "Social Media Manager",
-        foto: "../img/angela-lopez-social-media-manager.jpg",
+        foto: "angela-lopez-social-media-manager.jpg",
     },
     {
         nome: "Scott",
         cognome: "Estrada",
         ruolo: "Developer",
-        foto: "../img/scott-estrada-developer.jpg",
+        foto: "scott-estrada-developer.jpg",
     },
     {
         nome: "Barbara",
         cognome: "Ramos",
         ruolo: "Graphic Designer",
-        foto: "../img/barbara-ramos-graphic-designer.jpg",
+        foto: "barbara-ramos-graphic-designer.jpg",
     },
 ];
 
 console.log(students);
+
+const cardRow = document.querySelector(".row");
+
+let cards = "";
+for (let i = 0; i < students.length; i++) {
+    const curStudent = students[i];
+    cards += `
+            <div class="col">
+                <div class="card">
+                    <img src="img/${curStudent.foto}" class="card-img-top" alt="Foto">
+                    <div class="card-body">
+                        <h5 class="card-title text-center">${curStudent.nome} ${curStudent.cognome}</h5>
+                        <p class="card-text text-center">${curStudent.ruolo}</p>
+                    </div>
+                </div>
+            </div>
+        `;
+}
+console.log(cards);
+cardRow.innerHTML = cards;
